a3ccc3c21b43cb4af5f0f0b0beee315dcc5d6297
hieupham250 <147390672+hieupham250@users.noreply.github.com>
2024-01-26 07:45:59 +0700

    Add files via upload

---
+++ ss38+39/bai2.js
@@ -0,0 +1,12 @@
+let icon = document.getElementById("icon");
+let bar = document.getElementById("bar");
+icon.onclick = function () {
+    if(bar.style.width=="50px"){
+        bar.style.transition="2s";
+        bar.style.width="200px";}
+    
+    else{
+        bar.style.width="50px";
+        bar.style.transition="2s";
+    }
+}
